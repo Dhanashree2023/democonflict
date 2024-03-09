fdb6bf0a1a1e040ab693fcc7ec46225f3273e46f
Dhanashree2023 <Prachi123451@gmail.com>
2024-03-09 10:34:27 +0530

    developer y commit

--- src/democonflict/TestD.java
+++ src/democonflict/TestD.java
@@ -6,6 +6,12 @@ public class TestD {
 		
 		System.out.println("Test 1");
 		
+		String x = "Java";
+		String y = "Program";
+		System.out.println(x);
+		System.out.println(y);
+				
+		
 		
 		
 	}
